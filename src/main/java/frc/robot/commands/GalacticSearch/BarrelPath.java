2c4e01a8cdbfa471781a94a911212ab6d77eb33e
loreleibell <loreleibell1000@gmail.com>
2021-02-25 16:40:39 -0800

    BarrelPath Update
    Just made it work on the new robot

--- src/main/java/frc/robot/commands/GalacticSearch/BarrelPath.java
+++ src/main/java/frc/robot/commands/GalacticSearch/BarrelPath.java
@@ -33,18 +33,20 @@ public class BarrelPath extends ParallelCommandGroup {
 //4.5,25,90,2,2
 //0,25,-90,2,2
 //0,3.5,-90
+//changed
 
   static final Waypoint[] k_loopDrive = { 
-      new Waypoint(0, 3.5, Math.toRadians(90)),
+      new Waypoint(0, 3.5, Math.toRadians(90), 0, 0, 8),
       new Waypoint(0, 13, Math.toRadians(70)), 
       new Waypoint(5, 12.6, Math.toRadians(-90), 3, 3, 0), 
-      new Waypoint(0, 13, Math.toRadians(95)),
-      new Waypoint(-.75, 20, Math.toRadians(100)), 
+      new Waypoint(0, 13, Math.toRadians(95), 0, 0, 8),
+      new Waypoint(.5, 20, Math.toRadians(100)), 
       new Waypoint(-5, 20, Math.toRadians(-90), 2, 2, 0),
-      new Waypoint(-.75, 19, Math.toRadians(55), 1, 1, 0),
-      new Waypoint(4.5, 25, Math.toRadians(90), 2, 2, 0), 
-      new Waypoint(0, 25, Math.toRadians(-90), 2, 2, 0), 
-      new Waypoint(0, 3.5, Math.toRadians(-90))
+      new Waypoint(-.75, 19, Math.toRadians(55), 1, 1, 8),
+      new Waypoint(5.5, 25, Math.toRadians(90), 2, 2, 0), 
+      new Waypoint(1, 25, Math.toRadians(-90), 2, 2, 0),
+      new Waypoint(1, 20, Math.toRadians(-95), 1, 1, 15),
+      new Waypoint(-1.5, 2.5, Math.toRadians(-90))
      };
       
   DriveSubsystem m_driveSubsystem;
