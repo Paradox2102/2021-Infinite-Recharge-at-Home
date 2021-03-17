f190e831e08b19f43c086ff0a2a9a808d817d495
Ryder <36494378+Spork1023@users.noreply.github.com>
2021-03-17 13:03:54 -0700

    Added code for recieveing AI camera data

---
+++ src/main/java/frc/CameraReciever/Network.java
@@ -0,0 +1,64 @@
+package frc.CameraReciever;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class Network {
+    private String m_host; 
+    private int m_port; //1234
+
+    Region cameraData[];
+
+    public Network(String host, int port) {
+        m_host = host;
+        m_port = port;
+    }
+
+    public void Recieve() {
+        try {
+            byte command[] = new byte[4];
+            byte data[];
+            ByteBuffer commandBuffer = ByteBuffer.wrap(command);
+            ByteBuffer dataBuffer;
+            Socket sock = new Socket(m_host, m_port);
+
+            sock.getInputStream().read(command);
+
+            short numBalls;
+
+            if (commandBuffer.getShort() == 0x10) {
+                numBalls = commandBuffer.getShort();
+                data = new byte[numBalls * 8];
+                dataBuffer = ByteBuffer.wrap(data);
+                // cameraData = new short[numBalls][4];
+                cameraData = new Region[numBalls];
+
+
+                sock.getInputStream().read(data);
+
+                for (int i = 0; i < cameraData.length; i++) {
+                    cameraData[i] = new Region(dataBuffer.getShort(), dataBuffer.getShort(), dataBuffer.getShort(), dataBuffer.getShort());
+                }
+            } else {
+                cameraData = null;
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    
+    public Region[] getRegions() {
+        return cameraData;
+    }
+
+    public boolean canSee() {
+        return cameraData != null;
+    }    
+}
